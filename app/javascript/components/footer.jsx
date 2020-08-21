f40481c2c2317360a59e2e18ba75b21d795daa0f
slavam <mwm1955@gmail.com>
2020-08-21 14:48:27 +0300

    Изменены номера телефонов

--- app/javascript/components/footer.jsx
+++ app/javascript/components/footer.jsx
@@ -18,15 +18,15 @@ const Footer = () =>(
             г. Донецк
             ул. Любавина, дом 2
           </address>
-          <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br/>
-          <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br/>
+          <i className="fa fa-phone fa-lg"></i>: +38 050 288 9669<br/>
+          <i className="fa fa-fax fa-lg"></i>: +38 062 311 4010<br/>
           <i className="fa fa-envelope fa-lg"></i>: 
           <a href="mailto:mwm1955@gmail.com">mwm1955@gmail.com</a>
         </div>
         <div className='col-12 col-sm-4 align-self-center'>
           <div className="text-center">
-            <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
-            <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
+            {/* <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
+            <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a> */}
             <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
             <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
             <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
@@ -36,7 +36,7 @@ const Footer = () =>(
       </div>
       <div className='row justify-content-center'>
         <div className='col-auto'>
-          <p>© Copyright mwm 2020</p>
+          {/* <p>© Copyright mwm 2020</p> */}
         </div>
       </div>
     </div>
